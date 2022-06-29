a46ad0ac06ec25d663eda576fb31f998fb8985f4
riyazedd <riyaz.shrestha00@gmail.com>
2022-06-29 22:00:49 +0545

    practice

---
+++ Day 5/js/custom.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// --------for loop--------
+
+// let name=window.prompt("Enter your name");
+// for(let x=1; x<=10; x++){
+//     console.log(`${name} ${x}`);
+// }
+
+// -------10-1--------
+
+// for(let x=10; x>=1; x--){
+//     console.log(x);
+// }
+
+// ---------odd even----------
+
+// let a=0;
+// let b=0;
+// for(let x=1; x<=10; x++){
+//     if(x%2==0){
+//         console.log(x);
+//         a++;
+//     }
+// }
+// console.log(`Total number of even number is ${a}.`);
+
+// for(let x=1; x<=10; x++){
+//     if(x%2 !==0){
+//         console.log(x);
+//         b++;
+//     }
+// }
+// console.log(`Total number of odd number is ${b}.`);
+
+// for(let x=1; x<=10; x++){
+//     if(x==1 || x==5 || x==9){
+//         console.log(x);
+//     }
+// }
+
+// ---------while loop-----------
+
+// let x=1;
+// while(x<=10){
+//     console.log(x);
+//     x++;
+// }
