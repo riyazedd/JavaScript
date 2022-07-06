e6b6da457ec89019df071fead6bf2e0c4f2a313d
riyazedd <riyaz.shrestha00@gmail.com>
2022-07-06 16:31:28 +0545

    update

--- Day 6/hw/custom.js
+++ Day 6/hw/custom.js
@@ -2,13 +2,13 @@
 
 function details(){
     let n1= document.getElementById('name').value
-    let n2= document.getElementById('mail').value
+    let n2= document.getElementById('email').value
     let n3= document.getElementById('password').value
     let n4= document.getElementById('number').value
     
     document.getElementById('info').innerHTML=`<h1>Your Details:</h1>Name: ${n1} <br> Email: ${n2} <br> Number: ${n4}`;
     document.getElementById('name').value='';
-    document.getElementById('mail').value='';
+    document.getElementById('email').value='';
     document.getElementById('number').value='';
     document.getElementById('password').value='';
 }
